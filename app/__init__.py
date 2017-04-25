2c6e9cd4a4612762083b4c0b3bd405a10e032495
Zabieru <zabieru96@hotmail.com>
2017-04-25 07:19:20 +0000

    Adjusted JSON responses to match that of the API specification

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "fdcf3m3o(H*DNEDAIODNke*@@#NJEDed3"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:admin@localhost/web_project"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+UPLOAD_FOLDER = './app/static/uploads'
 
 db = SQLAlchemy(app)
 
+app.config.from_object(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -11,8 +11,8 @@ from flask import render_template, request, redirect, url_for, flash, jsonify
 from models import User, WishlistItem
 from forms import UserForm, WishlistForm
 from bs4 import BeautifulSoup
-import urllib2, requests
-
+from werkzeug.utils import secure_filename
+import urllib2, requests, os
 
 ###
 # Routing for your application.
@@ -25,58 +25,80 @@ def home():
 
 @app.route('/api/users/register', methods=['POST'])
 def register():
+    response = { "error": 'null', "data": {}, "message": "Success"}
     if request.method == 'POST':
+        file = request.files['picture']
+        if file:
+            file_folder = app.config['UPLOAD_FOLDER']
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(file_folder, filename))
+            name = filename
+        else:
+            name = 'default.jpg'
         data = request.form
-        user = User(data['email'], data['fname'], data['lname'], data['password'])
+        user = User(data['email'], data['name'], data['password'], name, data['age'], data['gender'])
         db.session.add(user)
         db.session.commit()
-        return jsonify({'status' : 'success', 'message' : 'User created successfully', 'user' : user.serialize})
-    return jsonify({'status' : 'error', 'message' : 'Error creating user'})
+        response["data"] = {"user" : user.serialize}
+        return jsonify(response)
+    response["error"] = "true"
+    response["message"] = "Error creating user"
+    return jsonify(response)
 
 @app.route('/api/users/login', methods=['POST'])
 def login():
+    response = { "error": 'null', "data": {}, "message": "Success"}
     if request.method == 'POST':
         data = request.form
         user = User.query.filter_by(email=data['email']).first()
-        response = {'status' : 'error', 'message' : 'Unknown', 'user' : 'null'}
         if user == None:
+            response['error'] = 'true'
             response['message'] = 'User does not exist.'
             return jsonify(response)
         elif user.password == data['password']:
-            response['status'] = 'success'
-            response['message'] = 'User logged in successfully.'
-            response['user'] = user.serialize
+            response['data'] = {'user' : user.serialize_many}
             return jsonify(response)
+    response['error'] = 'true'
     response['message'] = 'Invalid Password.'
     return jsonify(response)
 
 @app.route('/api/users/<int:userid>/wishlist', methods=['GET', 'POST'])
 def wishlist(userid):
+    response = { "error": 'null', "data": {}, "message": "Success"}
     user = User.query.get(userid)
     if user == None:
-        return jsonify({'status' : 'error', 'message' : 'User does not exist.'})
+        response['error'] = 'true'
+        response['message'] = 'User does not exist.'
+        return jsonify(response)
     if request.method == 'POST':
         data = request.form
-        wishlistitem = WishlistItem(data['name'], data['thumbnail'])
+        wishlistitem = WishlistItem(data['name'], data['thumbnail'], data['url'], data['desc'])
         user.wishlist.append(wishlistitem)
         db.session.add(user)
         db.session.commit()
-        return jsonify(status = 'success')
+        response['data'] = {"item" : wishlistitem.serialize}
+        return jsonify(response)
     itemlist = user.wishlist
-    print itemlist[0].user
-    return jsonify(wishlist = [item.serialize for item in itemlist])
+    if len([i for i in itemlist]) < 1:
+         response['error'] = 'true'
+         response['message'] = 'No wishlist exists'
+    else:
+        response['data'] = {"items" : [item.serialize for item in itemlist]}
+    return jsonify(response)
 
 @app.route('/api/thumbnails', methods=['GET'])
 def thumbnails():
     print urllib2.quote(request.args.get('url'))
     soup = BeautifulSoup(requests.get(request.args.get('url')).text, "lxml")
-    return jsonify(thumbnails = [img.get('src') for img in soup.find_all('img')])
+    response = { "error": 'null', "data": {"thumbnails": [img.get('src') for img in soup.find_all('img')]}, "message": "Success"}
+    return jsonify(response)
 
 @app.route('/api/users/<int:userid>/wishlist/<int:itemid>', methods=['DELETE'])
 def deleteitem(userid, itemid):
-    response = {'status' : 'error', 'message' : 'Unknown'}
+    response = { "error": 'null', "data": {}, "message": "Success"}
     user = User.query.get(userid)
     if user == None:
+        response['error'] = 'true'
         response['message'] = 'User not found'
         return jsonify(response)
     else:
@@ -84,8 +106,8 @@ def deleteitem(userid, itemid):
             if item.id == itemid:
                 user.wishlist.remove(item)
                 db.session.commit()
-                response = {'status' : 'success', 'message' : 'Item deleted successfully'}
                 return jsonify(response)
+    response['error'] = 'true'
     response['message'] = 'Item not found'
     return jsonify(response)
 
@@ -99,6 +121,13 @@ def send_text_file(file_name):
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
+def uploadfile(file):
+    file_folder = app.config['UPLOAD_FOLDER']
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(file_folder, filename)
+    file.save(file_path)
+    
+    return filename
 
 @app.after_request
 def add_header(response):
